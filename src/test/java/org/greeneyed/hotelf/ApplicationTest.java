9d8535cb40849ff3ef3ff31de07015bbd92623d2
verdoso <Verdoso@users.noreply.github.com>
2021-06-24 21:47:35 +0200

    Basic test needed fixing

--- src/test/java/org/greeneyed/hotelf/ApplicationTest.java
+++ src/test/java/org/greeneyed/hotelf/ApplicationTest.java
@@ -1,12 +1,18 @@
-package org.greeneyed.airportf;
+package org.greeneyed.hotelf;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.hotelbeds.distribution.hotel_api_sdk.HotelApiClient;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationTest {
+
+    @MockBean
+    HotelApiClient hotelApiClient;
 
     @Test
     void contextLoads() {
